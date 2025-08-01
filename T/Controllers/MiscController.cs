a0dae117ae33ebf7551a46690e1d84c039b34a80
overmet15 <metsocool675@gmail.com>
2025-08-01 12:08:11 +0900

    added misc controller for time

---
+++ T/Controllers/MiscController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace T.Controllers
+{
+    [Route("api/misc")]
+    [ApiController]
+    public class MiscController : ControllerBase
+    {
+        [HttpGet("getTime")]
+        public IActionResult GetTime()
+        {
+            DateTime time = DateTime.UtcNow;
+
+            JsonObject index = new()
+            {
+                ["year"] = time.Year,
+                ["month"] = time.Month,
+                ["day"] = time.Day,
+                ["hour"] = time.Hour,
+                ["minute"] = time.Minute,
+                ["second"] = time.Second
+            };
+
+            return Content(index.ToJsonString());
+        }
+    }
+}
